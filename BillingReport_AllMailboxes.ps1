30eafa8d5cc52bbe68bcedb5f649d13a93597d3d
jhu18 <124710328+jhu18@users.noreply.github.com>
2023-02-21 10:09:01 -0500

    cleanup comments

--- BillingReport_AllMailboxes.ps1
+++ BillingReport_AllMailboxes.ps1
@@ -1,25 +1,22 @@
 ﻿#Import the Azure AD module
 
 Import-Module ExchangeOnlineManagement
-Import-Module AzureAD
+Import-Module AzureAD # -UseWindowsPowerShell (need to be added on VB Code)
 
 Connect-ExchangeOnline
 Connect-AzureAD
-#Connect-MgGraph
 Connect-Graph -Scopes User.Read.All
 
 # Get a list of all users created in the past month
-#$Users = Get-Mailbox -ResultSize unlimited | Where-Object {[System.DateTime]$_.WhenMailboxCreated -gt $LastWeek}
-#$Mailboxes = Get-Mailbox -ResultSize unlimited | Where-Object {[System.DateTime]$_.WhenMailboxCreated -gt $LastWeek}
-#$Mailboxes = Get-EXOMailbox -ResultSize 100 -Properties  CustomAttribute5,Customattribute7,CustomAttribute14,WhenMailboxCreated, isMailboxEnabled, ArchiveStatus,ImmutableId
+
+# Get all mailboxes in the tenant
 $Mailboxes = Get-EXOMailbox -ResultSize unlimited -Properties  CustomAttribute5,Customattribute7,CustomAttribute14,WhenMailboxCreated, isMailboxEnabled, ArchiveStatus,ImmutableId
 
 $MailboxData = @()
 
 # Iterate through each user
 $Mailboxes | ForEach-Object {
-  #  Write-Host "Getting created date for" $_.UserPrincipalName
- 
+
     #Collect the user data
     $MailboxData += New-Object PSObject -property $([ordered]@{
 
@@ -45,10 +42,5 @@ $Mailboxes | ForEach-Object {
     })
 }
 
-
-
 #Export to CSV
-
-#$MailboxData | Export-Csv "C:\Temp\Office365UsersCreationHistory2.csv" -NoTypeInformation
 $MailboxData |Sort WhenMailboxCreated -Descending | Export-Csv "C:\scr\Jhu-test\WeeklyO365BillingReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv" -NoTypeInformation
-
